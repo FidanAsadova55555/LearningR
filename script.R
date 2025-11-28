db8a093333e3ae6e351bcd54ed2dd03668b1a17b
FidanAsadova55555 <fidanra@code.edu.az>
2025-11-28 20:18:41 +0400

    data is written

---
+++ script.R
@@ -0,0 +1,13 @@
+x <- 5
+first_name <- "Fidan"
+print(first_name)
+
+unit <- "kg"
+weight <- 48
+print(paste(weight,unit))
+sales_matrix <- matrix(
+    c(12000, 15000, 17000, 18000, 13000, 19000),
+    nrow = 2, 
+    ncol = 3
+)
+print(sales_matrix)
